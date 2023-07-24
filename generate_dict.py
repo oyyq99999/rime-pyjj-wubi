b2ca600fc5da18c2b7af0b4a5e3a65bf9d3083bf
Yunqi Ouyang <oyyq9999999@163.com>
2023-07-24 16:43:49 +0800

    add dict generator

---
+++ generate_dict.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+output_dir = 'generated'
+output_fn = 'caspal_pyjj_wubi.dict.yaml'
+
+header = '''# author: Caspal
+
+---
+name: caspal_pyjj_wubi
+version: "2023.07.24"
+sort: by_weight
+use_preset_vocabulary: false
+...
+
+'''
+
+def main():
+  pinyin_map = {}
+  fuma_map = {}
+  with open('{}/{}'.format(output_dir, 'caspal_pinyin.txt')) as f:
+    lines = f.readlines()
+  lines = [line.strip() for line in lines]
+  lines = list(filter(lambda x: len(x) > 0 and x[0] != '#', lines))
+  for line in lines:
+    ch, pinyin = line.split('\t')
+    pinyin_map.setdefault(ch, set()).add(pinyin)
+
+  with open('{}/{}'.format(output_dir, 'caspal_wubi86_fuma.txt')) as f:
+    lines = f.readlines()
+  lines = [line.strip() for line in lines]
+  lines = list(filter(lambda x: len(x) > 0 and x[0] != '#', lines))
+  for line in lines:
+    ch, fuma = line.split('\t')
+    fuma_map.setdefault(ch, set()).add(fuma)
+
+  for ch in fuma_map:
+    codes = fuma_map[ch].copy()
+    for a in fuma_map[ch]:
+      for b in fuma_map[ch]:
+        if a != b and b.startswith(a) and a in codes:
+          codes.remove(a)
+    fuma_map[ch] = codes
+
+  keys = sorted(pinyin_map.keys())
+  with open('{}/{}'.format(output_dir, output_fn), 'w') as f:
+    f.write(header)
+    for key in keys:
+      for pinyin in pinyin_map[key]:
+        if key in fuma_map:
+          for fuma in fuma_map[key]:
+            # print(key, pinyin, fuma)
+            f.write('{}\t{};{}\n'.format(key, pinyin, fuma))
+        else:
+          f.write('{}\t{}\n'.format(key, pinyin))
+
+
+if __name__ == '__main__':
+  main()
