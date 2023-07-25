48a4138238f5a76d5cc797612cebd28bd8b523fa
Yunqi Ouyang <oyyq9999999@163.com>
2023-07-25 16:05:13 +0800

    add wubi86 scheme and use it for reverse_lookup in pyjj_wubi schema

--- generate_dict.py
+++ generate_dict.py
@@ -2,6 +2,7 @@
 
 output_dir = 'generated'
 output_fn = 'caspal_pyjj_wubi.dict.yaml'
+output_fn_wubi = 'caspal_wubi86.dict.yaml'
 
 header = '''# author: Caspal
 
@@ -14,7 +15,55 @@ use_preset_vocabulary: true
 
 '''
 
-def main():
+wubi_header = '''# author: Caspal
+
+---
+name: caspal_wubi86
+version: "23.07.25"
+sort: by_weight
+columns:
+  - text
+  - code
+  - weight
+  - stem
+encoder:
+  exclude_patterns:
+    - '^z.*$'
+  rules:
+    - length_equal: 2
+      formula: "AaAbBaBb"
+    - length_equal: 3
+      formula: "AaBaCaCb"
+    - length_in_range: [4, 10]
+      formula: "AaBaCaZa"
+...
+
+'''
+
+def wubi():
+  wubi86_map = {}
+  with open ('{}/{}'.format(output_dir, 'caspal_wubi86.txt')) as f:
+    lines = f.readlines()
+  lines = [line.strip() for line in lines]
+  lines = list(filter(lambda x: len(x) > 0 and x[0] != '#', lines))
+  for line in lines:
+    ch, wubi = line.split('\t')
+    wubi86_map.setdefault(ch, set()).add(wubi)
+
+  keys = sorted(wubi86_map.keys())
+  with open('{}/{}'.format(output_dir, output_fn_wubi), 'w') as f:
+    f.write(wubi_header)
+    for key in keys:
+      for wubi in wubi86_map[key]:
+        f.write('{}\t{}\n'.format(key, wubi))
+    with open("{}/{}".format(output_dir, 'caspal_phrase_pinyin.txt')) as fph:
+      lines = fph.readlines()
+    f.write('\n')
+    f.write('# 以下为词组\n')
+    for line in lines:
+      f.write('{}\n'.format(line.split('\t')[0]))
+
+def pinyin_wubi():
   pinyin_map = {}
   fuma_map = {}
   with open('{}/{}'.format(output_dir, 'caspal_pinyin.txt')) as f:
@@ -59,7 +108,9 @@ def main():
     for line in lines:
       f.write('{}'.format(line))
 
-
+def main():
+  pinyin_wubi()
+  wubi()
 
 if __name__ == '__main__':
   main()
